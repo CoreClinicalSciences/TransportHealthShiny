a75d705f388f4465dc6bb5ccb5f46cfa5a983030
Quang Vuong <quang@coreclinicalsciences.com>
2024-07-11 12:25:01 -0700

    Started modifying UI to match mockup

--- server.R
+++ server.R
@@ -1,8 +1,12 @@
 library(shiny)
 library(shinydashboard)
 library(TransportHealthR)
+library(quarto)
+library(flextable)
 
 server <- function(input, output, session) {
+  
+  output$getStarted <- renderUI({suppressWarnings(includeHTML("GetStarted.html"))})
   
   studyData <- reactive({
     req(input$studyDataInput)
@@ -16,6 +20,11 @@ server <- function(input, output, session) {
   
   observeEvent(input$method, {
     if(input$method == "IOPW") {
+      output$dataUI <- renderUI({
+        fluidPage(
+          fileInput("studyDataInput", "Upload Study Data (CSV File)", accept = c(".csv")),
+          fileInput("targetDataInput", "Upload Target Data (CSV File)", accept = c(".csv")))
+      })
       output$modelUI <- renderUI({
         #Get shared variable names between the data sets
           req(studyData(), targetData())
@@ -23,15 +32,20 @@ server <- function(input, output, session) {
           
           ### Could add an error message here if the intersection is empty ###
         
-        fluidPage(
-          h3("Model Inputs"),
-          selectInput("response", "Specify the response variable", choices = names(studyData()), multiple = FALSE),
-          selectInput("treatment", "Specify the treatment variable", choices = names(studyData()), multiple = FALSE),
-          selectInput("propensityModel", "Specify Variables in Propensity Model", choices = names(studyData()), multiple = TRUE),
-          selectInput("participationModel", "Specify Variables in Participation Model", choices = commonVars, multiple = TRUE),
-          selectInput("msmModel", "Specify Variables in MSM", choices = names(studyData()), multiple = TRUE),
-          selectInput("responseType", "Specify the type of the response variable and regression model to be fit",
-                      choices = c("Continuous - Normal", "Binary - Logistic", "Survival - AFT", "Survival - Cox PH"))
+        fluidRow(
+          column(width = 4,
+            fluidPage(selectInput("response", "Specify the response variable", choices = names(studyData()), multiple = FALSE),
+                      selectInput("treatment", "Specify the treatment variable", choices = names(studyData()), multiple = FALSE)
+            )
+          ),
+          column(width = 4,
+            fluidPage(selectInput("propensityModel", "Specify Variables in Propensity Model", choices = names(studyData()), multiple = TRUE),
+                      selectInput("participationModel", "Specify Variables in Participation Model", choices = commonVars, multiple = TRUE),
+                      selectInput("msmModel", "Specify Variables in MSM", choices = names(studyData()), multiple = TRUE),
+                      selectInput("responseType", "Specify the type of the response variable and regression model to be fit",
+                                  choices = c("Continuous - Normal", "Binary - Logistic", "Ordinal - Logistic", "Survival - AFT", "Survival - Cox PH"))
+                      )
+          )
         )
       })
       
@@ -61,15 +75,18 @@ server <- function(input, output, session) {
       
       output$resultsUI <- renderUI({
         fluidPage(
-          h3("Results"),
-          tableOutput("resultsTable"),
+          h3(tags$b("Marginal structural model coefficient estimates")),
+          column(12, align = "center", tableOutput("resultsTable")),
+          column(12, align= "center", plotOutput("resultsPlot")),
+          h3(tags$b("Mirrored histograms")),
           fluidRow(
-            column(6, plotOutput("propensityHistOutput")),
-            column(6, plotOutput("participationHistOutput"))
+            column(6, fluidPage(plotOutput("propensityHistOutput"), span(p("Propensity", align = "center")))),
+            column(6, fluidPage(plotOutput("participationHistOutput"), span(p("Participation", align = "center"))))
           ),
+          h3(tags$b("Standardized mean differences plots")),
           fluidRow(
-            column(6, plotOutput("propensitySMDOutput")),
-            column(6, plotOutput("participationSMDOutput"))
+            column(6, fluidPage(plotOutput("propensitySMDOutput"), span(p("Propensity", align = "center")))),
+            column(6, fluidPage(plotOutput("participationSMDOutput"), span(p("Participation", align = "center"))))
           )
         )
       })
@@ -92,6 +109,10 @@ server <- function(input, output, session) {
       
       output$participationSMDOutput <- renderPlot({
         plot(resultIP(), type = "participationSMD")  # Placeholder plot
+      })
+      
+      output$resultsPlot <- renderPlot({
+        plot(resultIP(), type = "msm")
       })
     } else {
       output$modelUI <- renderUI({ NULL }) # Placeholder for the other methods' UI
